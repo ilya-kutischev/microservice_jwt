19db533e55a100beb89e12aa70074c84b958afd7
ilya228jpg <kutischev10@gmail.com>
2023-01-18 15:08:21 +0300

    AES KAFKA DONE

--- APIGateway/kafka_connector.py
+++ APIGateway/kafka_connector.py
@@ -1,7 +1,9 @@
 import asyncio
+import binascii
 import json
 from aiokafka import AIOKafkaConsumer, AIOKafkaProducer
 import logging
+from Cryptodome.Cipher import AES
 
 logger = logging.getLogger('uvicorn.info')
 """============================CONSUMER LOGIC==================================="""
@@ -21,9 +23,24 @@ class AsyncConsumer:
         try:
             async for msg in self.consumer:
                 logger.info(f'Message {msg} from {self.consumer}')
-                data = json.loads(msg.value)
+
+                topic = msg.topic
+                msg = msg.value
+                # here aes decoding of message
+                # generated secret key 512 bit, 32 byte, will migrate to .env
+                key = binascii.unhexlify("bb6eede04521f26fe160ca9f6f9930202b8b77cf3108368c4c90de0d9f8cc354")
+                tag = msg[:16]
+                nonce = msg[16:32]
+                ciphertext = msg[32:]
+                # logger.info(f"Chifred message: {tag, nonce, ciphertext} ")
+
+                cipher = AES.new(key, AES.MODE_EAX, nonce)
+                data = cipher.decrypt_and_verify(ciphertext, tag)
+
+
+                data = json.loads(data)
                 new_data = {key: value for (key, value) in data.items()}
-                if msg.topic == "recognizer_gateway":
+                if topic == "recognizer_gateway":
                     print(new_data)
         finally:
             await self.consumer.stop()
@@ -33,9 +50,21 @@ class AsyncConsumer:
         self.consumer.subscribe(pattern="recognizer_gateway")
         try:
             async for msg in self.consumer:
-                data = json.loads(msg.value)
+                topic = msg.topic
+                msg = msg.value
+                # here aes decoding of message
+                # generated secret key 512 bit, 32 byte, will migrate to .env
+                key = binascii.unhexlify("bb6eede04521f26fe160ca9f6f9930202b8b77cf3108368c4c90de0d9f8cc354")
+                tag = msg[:16]
+                nonce = msg[16:32]
+                ciphertext = msg[32:]
+                # logger.info(f"Chifred message: {tag, nonce, ciphertext} ")
+
+                cipher = AES.new(key, AES.MODE_EAX, nonce)
+                data = cipher.decrypt_and_verify(ciphertext, tag)
+                data = json.loads(data)
                 new_data = {key: value for (key, value) in data.items()}
-                if msg.topic == "recognizer_gateway" and new_data["request_id"] == request_id:
+                if topic == "recognizer_gateway" and new_data["request_id"] == request_id:
                     await self.consumer.stop()
                     return new_data["payload"]
         finally:
@@ -52,6 +81,16 @@ async def produce_message(topic="gateway_recognizer", msg=None):
                 "data": "used for adding data"
         }
     msg = json.dumps(msg).encode('utf-8')
+    # here aes encoding of message
+    #generated secret key 512 bit, 32 byte, will migrate to .env
+    key=binascii.unhexlify("bb6eede04521f26fe160ca9f6f9930202b8b77cf3108368c4c90de0d9f8cc354")
+    cipher = AES.new(key, AES.MODE_EAX)
+    ciphertext, tag = cipher.encrypt_and_digest(msg)
+    nonce = cipher.nonce
+    logger.info(f"Chifred message: {tag, nonce, ciphertext} ")
+    # doing byte concat
+    msg = tag + nonce + ciphertext
+
     producer = AIOKafkaProducer(
         loop=asyncio.get_running_loop(),
         bootstrap_servers="kafka:9092",
