7ef3a800cf2fb1e4ce49c10898db2d71392216fc
roman <roman.golubovich@gmail.com>
2023-01-29 20:28:56 +0300

    Revert "added simple password validation, added "/"in token endpoint and added isNone check for db_user"
    This reverts commit 409130409105b43dba8e82a93adab7c5721ff88e.

--- APIGateway/main.py
+++ APIGateway/main.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy.exc import IntegrityError
 from starlette.responses import Response
 
-import crud, models, schemas, security
+import crud, models, schemas,security
 from database import SessionLocal, engine
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi.middleware.cors import CORSMiddleware
@@ -18,18 +18,21 @@ from fastapi import APIRouter
 import base64
 
 from kafka_connector import produce_message, AsyncConsumer
-
 logger = logging.getLogger('uvicorn.info')
 
 router = APIRouter()
 
+
+
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
 
 models.Base.metadata.create_all(bind=engine)
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 app = FastAPI()
 
 app.include_router(router)
+
 
 origins = "*"
 
@@ -70,7 +73,7 @@ async def analyze_route(contents):
     request_id = str(uuid.uuid1())
     data_to_produce = {"payload": str(binascii.hexlify(contents)), "request_id": request_id}
     await produce_message("gateway_recognizer", data_to_produce)
-    # catching response
+    #catching response
     aio_consumer = AsyncConsumer()
     loop = asyncio.get_running_loop()
     response = await aio_consumer.consume_request(request_id)
@@ -78,23 +81,19 @@ async def analyze_route(contents):
 
 
 @app.post("/users/", response_model=schemas.User)
-def create_user(request: Request, user: schemas.UserCreate, db: Session = Depends(get_db)):
+def create_user(request:Request, user: schemas.UserCreate, db: Session = Depends(get_db)):
     # print(user.email)
     regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
     db_user = crud.get_user_by_email(db, email=user.email)
-    if re.fullmatch(regex, user.email) == None:
+    if re.fullmatch(regex, user.email)==None:
         raise HTTPException(status_code=409, detail="Invalid Email Address")
     if db_user:
         raise HTTPException(status_code=409, detail="Email already registered")
-
-    # simple password validation
-    if len(user.password.strip()) < 4:
-        raise HTTPException(status_code=409, detail="password too short")
     return crud.create_user(db=db, user=user)
 
 
 @app.get("/users/", response_model=List[schemas.User])
-def read_users(request: Request, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_users(request:Request, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     # return users
     return users
@@ -108,11 +107,11 @@ def read_user(user_id: int, db: Session = Depends(get_db)):
     return db_user
 
 
-@app.post("/token/", response_model=schemas.Token)
+@app.post("/token", response_model=schemas.Token)
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+
     db_user = crud.get_user_by_email(db, email=form_data.username)
-    if db_user is None or security.verify_hash(form_data.password, db_user.salt).decode(
-            'utf-8') != db_user.hashed_password:
+    if not (security.verify_hash(form_data.password,db_user.salt).decode('utf-8') == db_user.hashed_password):
         raise HTTPException(
             status_code=401,
             detail="Incorrect username or password",
@@ -126,9 +125,9 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
     return {"access_token": access_token}
 
 
-@app.get("/user", response_model=schemas.User)
+@app.get("/user",response_model=schemas.User)
 async def get_current_user(token: str, db: Session = Depends(get_db)):
-    email = security.get_current_user_email(token)
+    email=security.get_current_user_email(token)
     db_user = crud.get_user_by_email(db, email=email)
     print(db_user)
     return db_user
@@ -136,14 +135,14 @@ async def get_current_user(token: str, db: Session = Depends(get_db)):
 
 @app.get("/user/notes")
 async def get_current_user(access_token: str, db: Session = Depends(get_db)):
-    email = security.get_current_user_email(access_token)
+    email=security.get_current_user_email(access_token)
     db_user = crud.get_user_by_email(db, email=email)
 
-    return crud.get_user_notes(db, db_user)
+    return crud.get_user_notes(db,db_user)
 
 
 @app.get("/user/notes/pic")
-async def get_current_user(access_token: str, note_id: int, db: Session = Depends(get_db)):
+async def get_current_user(access_token: str, note_id:int, db: Session = Depends(get_db)):
     email = security.get_current_user_email(access_token)
     db_user = crud.get_user_by_email(db, email=email)
     note = crud.get_note_by_id(db, db_user, note_id)
@@ -156,18 +155,19 @@ async def get_current_user(access_token: str, note_id: int, db: Session = Depend
 
 @app.post("/user/notes")
 async def post_user_note(
-        request: Request,
-        access_token: str,
-        title: str,
-        description: str,
+        request:Request,
+        access_token:str,
+        title:str,
+        description:str,
         picture: UploadFile = File(...),
         db: Session = Depends(get_db)):
     # reading file
     picture = await picture.read()
-    print("TOKEN:", access_token)
-    email = security.get_current_user_email(access_token)
+    print("TOKEN:",access_token)
+    email=security.get_current_user_email(access_token)
     print("EMAIL DONE")
     db_user = crud.get_user_by_email(db, email=email)
+
 
     # не отправляем на распознавание картинку если у этого человека уже есть такие картинки в сохраненных
     notes = crud.get_note_by_pic(db, db_user, base64.b64encode(picture).decode('ASCII'))
@@ -179,47 +179,47 @@ async def post_user_note(
 
     # дальше создаем инстанс записки
     note = {
-        "title": title,
-        "description": description,
+        "title":title,
+        "description":description,
         # тут я уже не знаю какой тип подставить, мб хекс подойдет в бд или Бас 64
         "picture": str(base64.b64encode(picture).decode('ASCII')),
-        "latex": latex,
-        "owner_id": db_user.id
-    }
+        "latex":latex,
+        "owner_id":db_user.id
+            }
 
     try:
-        crud.create_user_note(db, note)
+        crud.create_user_note(db,note)
     except IntegrityError:
         db.rollback()
-        crud.update_user_note(db, note)
+        crud.update_user_note(db,note)
 
-    return {"message": note}
+    return {"message":note}
 
 
 @app.put("/user/notes")
-async def update_user_note(request: Request, access_token: str, note_id: int, title: str, description: str,
-                           db: Session = Depends(get_db)):
-    email = security.get_current_user_email(access_token)
+async def update_user_note(request:Request,access_token:str,note_id:int,title:str, description:str,db: Session = Depends(get_db)):
+    email=security.get_current_user_email(access_token)
     db_user = crud.get_user_by_email(db, email=email)
     note = {
-        "id": note_id,
+        "id":note_id,
         "title": title,
         "description": description,
         "owner_id": db_user.id
     }
-    crud.update_user_note(db, note)
-    return {"message": note}
+    crud.update_user_note(db,note)
+    return {"message":note}
 
 
 @app.delete("/user/notes")
-async def delete_user_items(request: Request, access_token: str, note_id: int, db: Session = Depends(get_db)):
-    email = security.get_current_user_email(access_token)
+async def delete_user_items(request:Request,access_token:str,note_id:int,db: Session = Depends(get_db)):
+    email=security.get_current_user_email(access_token)
     db_user = crud.get_user_by_email(db, email=email)
     note = {
         "id": note_id,
         "owner_id": db_user.id
     }
     print(note)
-    crud.delete_user_note(db, note)
+    crud.delete_user_note(db,note)
 
-    return {"message": note}
+    return {"message":note}
+    
